f4f6ae95e3f6f375a1d2bfee923e68756c736a2f
hyhgo0903 <72858069+hyhgo0903@users.noreply.github.com>
2021-09-03 17:35:50 +0900

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpSample
+{
+    internal sealed class A
+    {
+        public bool Value;
+    }
+
+    internal sealed class B
+    {
+        public A Data;
+    }
+
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            var b = new B();
+
+            if (b?.Data?.Value != false)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
