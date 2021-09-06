128964562c0fc09f97326311d1be85fe7c1c7ac4
hyhgo0903 <hyhgo0903@gmail.com>
2021-09-06 14:42:30 +0900

    더미 테스트 추가

--- ConsoleApp3/TestProject1/UnitTest1.cs
+++ ConsoleApp3/TestProject1/UnitTest1.cs
@@ -5,10 +5,14 @@ namespace TestProject1
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod] public void TestMethod1()
         {
             Assert.AreEqual(1, 1);
         }
+
+        [TestMethod] public void DummyTest1() { }
+        [TestMethod] public void DummyTest2() { }
+        [TestMethod] public void DummyTest3() { }
+        [TestMethod] public void DummyTest4() { }
     }
 }
