07eb51baaa21f8074294b7e5a1fbba683d9e01da
hyhgo0903 <hyhgo0903@gmail.com>
2021-09-06 11:00:19 +0900

    유닛테스트 실패 푸시

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -2,21 +2,11 @@
 
 namespace CSharpSample
 {
-    internal sealed class A
-    {
-        public bool Value;
-    }
-
-    internal sealed class B
-    {
-        public A Data;
-    }
-
     class Program
     {
         static void Main(string[] _)
         {
-            throw new Exception();
+            return;
         }
     }
 }
